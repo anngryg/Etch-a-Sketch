506728f146e09a8af17a205b506fef2770b0a9bd
Anna <anna.grygierczyk@wp.pl>
2023-08-15 14:42:54 +0200

    rainbow paint and eraser added

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let board = document.querySelector('.board');
+const board = document.querySelector('.board');
+let color = '#ffc0cb';
+const pen = document.querySelector('input#pen');
 
 /*slider value*/
-const slider = document.querySelector('#boardSize');
-const value = document.querySelector('.value');
-value.textContent = slider.value;
+const slider = document.querySelector('input#boardSize');
+const sliderValue = document.querySelector('div.value');
+sliderValue.textContent = slider.value;
 slider.oninput = function(){
-    value.textContent = this.value;
+    sliderValue.textContent = slider.value;
 }
+
+/*pen color picker*/
+
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
     createDivBoard(16);
@@ -16,16 +23,14 @@ document.addEventListener('DOMContentLoaded', function(){
 let boardSizeBtn = document.querySelector('#boardSizeBtn');
 boardSizeBtn.addEventListener('click', function(){
     let inputSize;
-    inputSize=value.textContent;
-    console.log(inputSize);
-    reset();
+    inputSize=sliderValue.textContent;
     createDivBoard(inputSize);
 })
-
 
 function createDivBoard(size){
     let divs = board.querySelectorAll('div')
     divs.forEach((div) => div.remove()); /*removing all previously made divs, clearing the board*/ 
+    
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
@@ -33,14 +38,44 @@ function createDivBoard(size){
     for(let i=0; i<divNum; i++){
         let div = document.createElement('div');
         div.classList.add('cells');
-        board.insertAdjacentElement('beforeend', div)
-        div.addEventListener('mouseover', ()=>{
-            div.style.backgroundColor = 'pink'
-        }
-        )
+        board.insertAdjacentElement('beforeend', div);
+        div.addEventListener('mouseover', colorDiv);
     }
 }
 
-function reset(){
-    createDivBoard(1)
+function colorDiv() {
+    if(color == 'random'){
+        this.style.backgroundColor= randomColor();
+    }else if (color=='reset'){
+        this.style.backgroundColor = '#ffc0cb';
+    }else if (color=='myColor'){
+        this.style.backgroundColor = penInput();
+    }else if (color=='eraser'){
+        this.style.backgroundColor = '#353535';
+    }else{
+        this.style.backgroundColor = '#ffc0cb';
+    }
+}
+
+function chooseColor(colorChoice){
+    color= colorChoice;
+}
+
+function penInput(){
+    return pen.value;
+}
+
+
+function randomColor(){
+
+    let min = 0;
+    let max = 255;
+     a = Math.floor(min + Math.random() * (max - min + 1));
+     b = Math.floor(min + Math.random() * (max - min + 1));
+     c = Math.floor(min + Math.random() * (max - min + 1));
+
+     let randomColor = `rgb(${a}, ${b}, ${c})`;
+     return randomColor;
+   
+     
 }
